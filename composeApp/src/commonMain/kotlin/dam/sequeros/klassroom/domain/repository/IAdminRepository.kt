3e760092774003095a32e9204e474e253ab57fbb
jose antonio ruiz rives <ruizrivesjoseantonio@gmail.com>
2026-05-28 21:46:25 +0200

    Cambios en Iadmin duplicado.

--- composeApp/src/commonMain/kotlin/dam/sequeros/klassroom/domain/repository/IAdminRepository.kt
+++ composeApp/src/commonMain/kotlin/dam/sequeros/klassroom/domain/repository/IAdminRepository.kt
@@ -1,14 +1,10 @@
 package dam.sequeros.klassroom.domain.repository
 
 import dam.sequeros.klassroom.aplication.command.AddCurseCommand
+import dam.sequeros.klassroom.aplication.command.AddSubjectCommand
 import dam.sequeros.klassroom.domain.model.Course
 
 interface IAdminRepository {
     suspend fun addCourse(command: AddCurseCommand): Boolean
     suspend fun getCourses(): List<Course>
-import dam.sequeros.klassroom.aplication.command.AddSubjectCommand
-
-interface IAdminRepository {
-    suspend fun addCourse(command: AddCurseCommand): Boolean
-    suspend fun addSubject(command: AddSubjectCommand): Boolean
-}
+    suspend fun addSubject(command: AddSubjectCommand): Boolean}
